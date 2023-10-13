c0610fcea394b8b8287049f80de025e7c3dc15a4
GabrielaCS13 <gabriela.silva.00243@gmail.com>
2023-10-13 19:23:09 -0300

    adicionando

---
+++ src/App.js
@@ -0,0 +1,138 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div style={{width: 1512, height: 982, position: 'relative', background: 'white'}}>
+  <div style={{width: 255, height: 34, left: 746, top: 39, position: 'absolute'}}>
+    <div style={{width: 255, height: 34, left: 0, top: 0, position: 'absolute', background: '#F5F5F5', borderRadius: 6}} />
+    <div style={{width: 18, height: 18, left: 221, top: 8, position: 'absolute'}}>
+      <div style={{width: 12, height: 12, left: 2.25, top: 2.25, position: 'absolute', border: '1px black solid'}}></div>
+      <div style={{width: 3.26, height: 3.26, left: 12.49, top: 12.49, position: 'absolute', border: '1px black solid'}}></div>
+    </div>
+    <div style={{width: 73, height: 16, left: 18, top: 9, position: 'absolute', color: 'rgba(0, 0, 0, 0.65)', fontSize: 14, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Procurar</div>
+  </div>
+  <div style={{width: 242, height: 37, left: 310, top: 173, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Mulish', fontWeight: '700', wordWrap: 'break-word'}}>Cadastro</div>
+  <div style={{width: 168.78, height: 33, left: 1271, top: 39, position: 'absolute', background: 'black'}}></div>
+  <div style={{width: 210, height: 0, left: 12, top: 204, position: 'absolute'}}></div>
+  <div style={{width: 1079, height: 0, left: 317, top: 240, position: 'absolute', border: '1px #E2E2E2 solid'}}></div>
+  <div style={{width: 405, height: 59, left: 349, top: 367, position: 'absolute', background: '#F5F5F5', borderRadius: 6}} />
+  <div style={{width: 358, height: 59, left: 905, top: 367, position: 'absolute', background: '#F5F5F5', borderRadius: 6}} />
+  <div style={{width: 405, height: 59, left: 349, top: 472, position: 'absolute', background: '#F5F5F5', borderRadius: 6}} />
+  <div style={{width: 219, height: 37, left: 345, top: 269, position: 'absolute', color: 'black', fontSize: 32, fontFamily: 'Mulish', fontWeight: '600', wordWrap: 'break-word'}}>Atração</div>
+  <div style={{left: 349, top: 342, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Mulish', fontWeight: '400', wordWrap: 'break-word'}}>*Nome: </div>
+  <div style={{left: 905, top: 342, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Mulish', fontWeight: '400', wordWrap: 'break-word'}}>*Tipo: </div>
+  <div style={{left: 349, top: 448, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Mulish', fontWeight: '400', wordWrap: 'break-word'}}>*Descrição: </div>
+  <div style={{width: 174, height: 59, left: 1054, top: 865, position: 'absolute'}}>
+    <div style={{width: 174, height: 59, left: 0, top: 0, position: 'absolute', background: '#FFD121', borderRadius: 6}} />
+    <div style={{left: 45, top: 18, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Cancelar</div>
+  </div>
+  <div style={{width: 174, height: 59, left: 1256, top: 865, position: 'absolute'}}>
+    <div style={{width: 174, height: 59, left: 0, top: 0, position: 'absolute', background: '#AADE26', borderRadius: 6}} />
+    <div style={{left: 58, top: 18, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Salvar</div>
+  </div>
+  <div style={{width: 8, height: 4, left: 1229, top: 395, position: 'absolute', border: '1.70px black solid'}}></div>
+  <div style={{width: 59, height: 59, left: 1280, top: 367, position: 'absolute', background: '#F5F5F5', borderRadius: 6}} />
+  <div style={{width: 59, height: 59, left: 1356, top: 367, position: 'absolute', background: '#F5F5F5', borderRadius: 6}} />
+  <div style={{width: 19, height: 19, left: 1300, top: 387, position: 'absolute'}}>
+    <div style={{width: 16.89, height: 16.89, left: 0, top: 0, position: 'absolute', border: '1.41px black solid'}}></div>
+    <div style={{width: 4.59, height: 4.59, left: 14.41, top: 14.41, position: 'absolute', border: '1.41px black solid'}}></div>
+  </div>
+  <div style={{width: 19, height: 19, left: 1376, top: 387, position: 'absolute', background: 'black'}}></div>
+  <div style={{width: 510, height: 154, left: 905, top: 452, position: 'absolute', borderRadius: 6, border: '1px black solid'}} />
+  <div style={{width: 413.68, height: 78, left: 953, top: 490, position: 'absolute'}}>
+    <div style={{width: 355, height: 42, left: 0, top: 36, position: 'absolute', background: '#F5F5F5', borderRadius: 6}} />
+    <div style={{width: 162, left: 0, top: 0, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Mulish', fontWeight: '400', wordWrap: 'break-word'}}>Tipo Turismo: </div>
+    <div style={{width: 44.68, height: 42, left: 369, top: 36, position: 'absolute'}}>
+      <div style={{width: 44.68, height: 42, left: 0, top: 0, position: 'absolute', background: '#AADE26', borderRadius: 5.36}} />
+      <div style={{width: 16.98, height: 16.98, left: 14.30, top: 12.51, position: 'absolute'}}>
+        <div style={{width: 10.61, height: 8.49, left: 5.66, top: 2.12, position: 'absolute', background: 'black'}}></div>
+        <div style={{width: 14.15, height: 14.15, left: 1.41, top: 1.41, position: 'absolute', background: 'black'}}></div>
+      </div>
+    </div>
+  </div>
+  <div style={{width: 206.38, height: 35, left: 320, top: 38, position: 'absolute'}}>
+    <div style={{width: 39.83, height: 35, left: 56, top: 0, position: 'absolute'}}>
+      <div style={{width: 39.83, height: 35, left: 0, top: 0, position: 'absolute', background: '#FDE964', borderRadius: 4.83, border: '1px #757575 solid'}} />
+      <div style={{width: 24.14, height: 24.14, left: 8, top: 5, position: 'absolute'}}>
+        <div style={{width: 24.14, height: 24.14, left: 0, top: 0, position: 'absolute', border: '2.41px #333333 solid'}}></div>
+        <div style={{width: 10.86, height: 21.72, left: 1.21, top: 1.21, position: 'absolute', background: '#333333'}}></div>
+      </div>
+    </div>
+    <div style={{width: 39.83, height: 35, left: 111.03, top: 0, position: 'absolute'}}>
+      <div style={{width: 39.83, height: 35, left: 0, top: 0, position: 'absolute', background: '#FDE964', borderRadius: 4.83, border: '1px #757575 solid'}} />
+      <div style={{width: 27.76, height: 26.55, left: 8.45, top: 4.83, position: 'absolute', color: '#333333', fontSize: 19.31, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>A+</div>
+    </div>
+    <div style={{width: 39.83, height: 35, left: 166.55, top: 0, position: 'absolute'}}>
+      <div style={{width: 39.83, height: 35, left: 0, top: 0, position: 'absolute', background: '#FDE964', borderRadius: 4.83, border: '1px #757575 solid'}} />
+      <div style={{width: 27.76, height: 26.55, left: 8.45, top: 4.83, position: 'absolute', color: '#333333', fontSize: 19.31, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>A-</div>
+    </div>
+    <div style={{width: 39.83, height: 35, left: 0, top: 0, position: 'absolute'}}>
+      <div style={{width: 39.83, height: 35, left: 0, top: 0, position: 'absolute', background: '#FDE964', borderRadius: 4.83, border: '1px #757575 solid'}} />
+      <div style={{width: 26.55, height: 26.55, left: 7, top: 4, position: 'absolute'}}>
+        <div style={{width: 12.07, height: 12.07, left: 7.24, top: 7.24, position: 'absolute', border: '2.41px #333333 solid'}}></div>
+        <div style={{width: 0, height: 2.41, left: 13.28, top: 0, position: 'absolute', border: '2.41px #333333 solid'}}></div>
+        <div style={{width: 0, height: 2.41, left: 13.28, top: 24.14, position: 'absolute', border: '2.41px #333333 solid'}}></div>
+        <div style={{width: 1.71, height: 1.71, left: 3.89, top: 3.89, position: 'absolute', border: '2.41px #333333 solid'}}></div>
+        <div style={{width: 1.71, height: 1.71, left: 20.95, top: 20.95, position: 'absolute', border: '2.41px #333333 solid'}}></div>
+        <div style={{width: 2.41, height: 0, left: 0, top: 13.28, position: 'absolute', border: '2.41px #333333 solid'}}></div>
+        <div style={{width: 2.41, height: 0, left: 24.14, top: 13.28, position: 'absolute', border: '2.41px #333333 solid'}}></div>
+        <div style={{width: 1.71, height: 1.71, left: 3.89, top: 20.95, position: 'absolute', border: '2.41px #333333 solid'}}></div>
+        <div style={{width: 1.71, height: 1.71, left: 20.95, top: 3.89, position: 'absolute', border: '2.41px #333333 solid'}}></div>
+      </div>
+    </div>
+  </div>
+  <div style={{width: 246, height: 982, left: 0, top: 0, position: 'absolute'}}>
+    <div style={{width: 246, height: 982, left: 0, top: 0, position: 'absolute', background: 'black'}} />
+    <div style={{width: 19.66, height: 19.66, left: 24, top: 927, position: 'absolute'}}>
+      <div style={{width: 6.55, height: 16.38, left: 11.47, top: 1.64, position: 'absolute', background: '#F3F3F3'}}></div>
+      <div style={{width: 5.73, height: 9.83, left: 7.37, top: 4.91, position: 'absolute', background: '#F3F3F3'}}></div>
+      <div style={{width: 11.47, height: 1.64, left: 1.64, top: 9.01, position: 'absolute', background: '#F3F3F3'}}></div>
+    </div>
+    <div style={{width: 50, left: 52, top: 926, position: 'absolute', color: '#F3F3F3', fontSize: 18, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Sair</div>
+    <div style={{width: 200, height: 0, left: 22, top: 918, position: 'absolute', border: '1px rgba(244, 244, 244, 0.96) solid'}}></div>
+    <div style={{width: 205.44, height: 208.73, left: 31, top: 381, position: 'absolute'}}>
+      <div style={{width: 85.64, height: 19.66, left: 36.50, top: 0, position: 'absolute', color: '#F3F3F3', fontSize: 18, fontFamily: 'Inter', fontWeight: '600', wordWrap: 'break-word'}}>Usuários</div>
+      <div style={{width: 168.93, height: 20.37, left: 36.50, top: 36.50, position: 'absolute', color: '#F3F3F3', fontSize: 18, fontFamily: 'Inter', fontWeight: '600', wordWrap: 'break-word'}}>Ponto Turístico</div>
+      <div style={{width: 146.01, height: 19.66, left: 36.50, top: 73.01, position: 'absolute', color: '#F3F3F3', fontSize: 18, fontFamily: 'Inter', fontWeight: '600', wordWrap: 'break-word'}}>Eventos</div>
+      <div style={{width: 146.01, height: 19.66, left: 35.10, top: 109.51, position: 'absolute', color: '#F3F3F3', fontSize: 18, fontFamily: 'Inter', fontWeight: '600', wordWrap: 'break-word'}}>Atrações</div>
+      <div style={{width: 146.01, left: 35.10, top: 146.01, position: 'absolute', color: '#F3F3F3', fontSize: 18, fontFamily: 'Inter', fontWeight: '600', wordWrap: 'break-word'}}>Notícias</div>
+      <div style={{width: 146.01, left: 35.10, top: 186.73, position: 'absolute', color: '#F3F3F3', fontSize: 18, fontFamily: 'Inter', fontWeight: '600', wordWrap: 'break-word'}}>Dashboard</div>
+      <div style={{width: 15.04, height: 15.45, left: 1.40, top: 190.94, position: 'absolute'}}>
+        <div style={{width: 2.20, height: 9.83, left: 6.42, top: 5.62, position: 'absolute', background: '#F3F3F3'}}></div>
+        <div style={{width: 2.20, height: 15.44, left: 12.83, top: 0, position: 'absolute', background: '#F3F3F3'}}></div>
+        <div style={{width: 2.20, height: 5.62, left: -0, top: 9.83, position: 'absolute', background: '#F3F3F3'}}></div>
+      </div>
+      <div style={{width: 18.25, height: 16.85, left: 0, top: 4.21, position: 'absolute'}}>
+        <div style={{width: 14.04, height: 7.02, left: 0, top: 9.83, position: 'absolute', background: '#F3F3F3'}}></div>
+        <div style={{width: 8.42, height: 8.42, left: 2.81, top: 0, position: 'absolute', background: '#F3F3F3'}}></div>
+        <div style={{width: 4.21, height: 7.02, left: 14.04, top: 9.83, position: 'absolute', background: '#F3F3F3'}}></div>
+        <div style={{width: 4.21, height: 8.42, left: 11.23, top: 0, position: 'absolute', background: '#F3F3F3'}}></div>
+      </div>
+      <div style={{width: 16.85, height: 16.85, left: 1.40, top: 39.31, position: 'absolute'}}>
+        <div style={{width: 12.64, height: 16.85, left: 0, top: 0, position: 'absolute', background: '#F3F3F3'}}></div>
+        <div style={{width: 5.05, height: 6.74, left: 11.79, top: 10.11, position: 'absolute', background: '#F3F3F3'}}></div>
+      </div>
+      <div style={{width: 7.02, height: 2.81, left: 121, top: 10.64, position: 'absolute', border: '1.40px #F3F3F3 solid'}}></div>
+      <div style={{width: 16.85, height: 16.85, left: 1.40, top: 149.17, position: 'absolute'}}>
+        <div style={{width: 16.85, height: 16.85, left: 0, top: 0, position: 'absolute', background: '#F3F3F3'}}></div>
+        <div style={{width: 12.61, height: 12.06, left: 1.01, top: 1.96, position: 'absolute', background: '#F3F3F3'}}></div>
+      </div>
+      <div style={{width: 12.64, height: 15.44, left: 2.81, top: 112.32, position: 'absolute', background: '#F3F3F3'}}></div>
+      <div style={{width: 14.04, height: 15.21, left: 2.11, top: 77.22, position: 'absolute', background: '#F3F3F3'}}></div>
+    </div>
+    <div style={{width: 200, height: 0, left: 22, top: 340, position: 'absolute', border: '1px rgba(244, 244, 244, 0.96) solid'}}></div>
+    <div style={{width: 157, height: 29, left: 44, top: 271, position: 'absolute', color: '#F4F4F4', fontSize: 24, fontFamily: 'Inter', fontWeight: '600', wordWrap: 'break-word'}}>Rodrigo Faro</div>
+    <div style={{width: 100, height: 100, left: 72, top: 144, position: 'absolute', borderRadius: '50px' , border: '2px white solid'}}></div>
+   
+    <img style={{width: 66, height: 44, left: 120, top: 210, position: 'absolute'}} src="logoGlobo.png" />
+  </div>
+</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
